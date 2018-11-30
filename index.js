49456213d35727c1507ac4fc71f170c48e7e8c6e
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-30 01:15:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = (i + n).toString()
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
